d1bbbd705b7c5d1bce1b74c25d3a19405475fa93
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-03 23:46:05 -0700

    AdsController #1
    Update method in AdsController in Ads.Command.Api

--- Ads/src/Command/Ads.Command.Api/Controllers/AdsController.cs
+++ Ads/src/Command/Ads.Command.Api/Controllers/AdsController.cs
@@ -2,6 +2,7 @@ using Ads.Command.Application.Features.CreateClassifiedAd;
 using Ads.Command.Application.Features.DeleteClassifiedAd;
 using Ads.Command.Application.Features.PublishClassifiedAd;
 using Ads.Command.Application.Features.UnpublishClassifiedAd;
+using Ads.Command.Application.Features.UpdateClassifiedAd;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ads.Command.Api.Controllers;
@@ -81,6 +82,28 @@ public class AdsController : BaseApiController
         );
 
         UnpublishClassifiedAdResponse response = await Mediator
+            .Send(command, cancellationToken);
+
+        return Ok(response);
+    }
+
+    [HttpPut("{ClassifiedAdId}")]
+    public async Task<IActionResult> Update(
+        string classifiedAdId,
+        [FromBody] UpdateClassifiedAdRequestResource resource,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateClassifiedAdCommand(
+            new UpdateClassifiedAdRequest
+            {
+                ClassifiedAdId = classifiedAdId,
+                Description = resource.Description,
+                ExpectedVersion = resource.ExpectedVersion,
+                Title = resource.Title
+            }
+        );
+
+        UpdateClassifiedAdResponse response = await Mediator
             .Send(command, cancellationToken);
 
         return Ok(response);
