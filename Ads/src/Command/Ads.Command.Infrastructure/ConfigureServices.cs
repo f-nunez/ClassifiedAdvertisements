813b0fc214db7b23892bb434130b42bbfa902f81
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-03 22:50:12 -0700

    ConfigureServices #1
    ConfigureServices in Ads.Command.Infrastructure

---
+++ Ads/src/Command/Ads.Command.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,80 @@
+using Ads.Command.Application.Common.Interfaces;
+using Ads.Command.Domain.ClassifiedAdAggregate;
+using Ads.Command.Infrastructure.EventStores;
+using Ads.Command.Infrastructure.Persistence.Repositories;
+using Ads.Command.Infrastructure.ServiceBus;
+using Ads.Command.Infrastructure.ServiceBus.Observers;
+using Ads.Command.Infrastructure.Settings;
+using EventStore.Client;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddEventStoreDb(configuration);
+
+        services.AddMassTransit(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddEventStoreDb(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IServiceBus, MassTransitServiceBus>();
+
+        services.AddSingleton(
+            new EventStoreClient(
+                EventStoreClientSettings.Create(
+                    configuration.GetConnectionString("EventStoreConnection")!)
+            )
+        );
+
+        services.AddScoped<IEventStore<ClassifiedAd>, EventStore<ClassifiedAd>>();
+
+        services.AddScoped<IRepository, Repository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddMassTransit(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IRabbitMqSetting>(configuration
+            .GetSection(typeof(RabbitMqSetting).Name)
+            .Get<RabbitMqSetting>()!);
+
+        services.AddScoped<IServiceBus, MassTransitServiceBus>();
+
+        services.AddConsumeObserver<LoggingConsumeObserver>();
+
+        services.AddPublishObserver<LoggingPublishObserver>();
+
+        services.AddReceiveObserver<LoggingReceiveObserver>();
+
+        services.AddSendObserver<LoggingSendObserver>();
+
+        services.AddMassTransit(mt =>
+        {
+            mt.UsingRabbitMq((context, cfg) =>
+            {
+                var rabbitMqSetting = context
+                    .GetRequiredService<IRabbitMqSetting>();
+
+                cfg.Host(rabbitMqSetting.HostAddress);
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
