c83e8868ea46cdbd1fe9388fe5f8ec0c67b329fa
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-03 23:43:20 -0700

    AdsController #1
    Create method in AdsController in Ads.Command.Api

--- Ads/src/Command/Ads.Command.Api/Controllers/AdsController.cs
+++ Ads/src/Command/Ads.Command.Api/Controllers/AdsController.cs
@@ -1,5 +1,26 @@
+using Ads.Command.Application.Features.CreateClassifiedAd;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Ads.Command.Api.Controllers;
 
 public class AdsController : BaseApiController
 {
+    [HttpPost()]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateClassifiedAdRequestResource resource,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateClassifiedAdCommand(
+            new CreateClassifiedAdRequest
+            {
+                Description = resource.Description,
+                Title = resource.Title
+            }
+        );
+
+        CreateClassifiedAdResponse response = await Mediator
+            .Send(command, cancellationToken);
+
+        return Ok(response);
+    }
 }
