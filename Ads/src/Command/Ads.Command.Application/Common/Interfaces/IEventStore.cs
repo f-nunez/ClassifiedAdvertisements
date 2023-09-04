c3fc9db4ee6607f52fe3e2ef5fd58c544c299a94
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-03 22:44:28 -0700

    IEventStore #1
    Add CancellationToken as default parameter in Ads.Command.Application.Common.Interfaces

--- Ads/src/Command/Ads.Command.Application/Common/Interfaces/IEventStore.cs
+++ Ads/src/Command/Ads.Command.Application/Common/Interfaces/IEventStore.cs
@@ -4,6 +4,6 @@ namespace Ads.Command.Application.Common.Interfaces;
 
 public interface IEventStore<T> where T : IAggregateRoot
 {
-    Task AppendEventsAsync(T aggregateRoot, long expectedVersion, Guid correlationId);
-    Task<T> ReadStreamEventsAsync<TId>(TId id);
+    Task AppendEventsAsync(T aggregateRoot, long expectedVersion, Guid correlationId, CancellationToken cancellationToken = default);
+    Task<T> ReadStreamEventsAsync<TId>(TId id, CancellationToken cancellationToken = default);
 }
