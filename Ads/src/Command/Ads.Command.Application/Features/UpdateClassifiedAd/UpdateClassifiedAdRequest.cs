ee715e189e7963128d84a7a912195e5187416831
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-05 14:41:21 -0700

    UpdateClassifiedAdRequest #1
    Update properties as nullable into UpdateClassifiedAdRequest in Ads.Command.Application

--- Ads/src/Command/Ads.Command.Application/Features/UpdateClassifiedAd/UpdateClassifiedAdRequest.cs
+++ Ads/src/Command/Ads.Command.Application/Features/UpdateClassifiedAd/UpdateClassifiedAdRequest.cs
@@ -4,8 +4,8 @@ namespace Ads.Command.Application.Features.UpdateClassifiedAd;
 
 public class UpdateClassifiedAdRequest : BaseRequest
 {
-    public string ClassifiedAdId { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public long ExpectedVersion { get; set; } = -1;
-    public string Title { get; set; } = string.Empty;
+    public string? ClassifiedAdId { get; set; }
+    public string? Description { get; set; }
+    public long ExpectedVersion { get; set; }
+    public string? Title { get; set; }
 }
