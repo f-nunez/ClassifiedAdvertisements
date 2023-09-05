2de85c55335cdb8a1e62468966af225a7acaa542
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-04 21:26:39 -0700

    AdsController #1
    AdsController in Ads.Query.Api

---
+++ Ads/src/Query/Ads.Query.Api/Controllers/AdsController.cs
@@ -0,0 +1,25 @@
+using Ads.Query.Application.Features.GetClassifiedAd;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ads.Query.Api.Controllers;
+
+public class AdsController : BaseApiController
+{
+    [HttpGet("{classifiedAdId}")]
+    public async Task<IActionResult> Get(
+        string classifiedAdId,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetClassifiedAdQuery(
+            new GetClassifiedAdRequest
+            {
+                ClassifiedAdId = classifiedAdId
+            }
+        );
+
+        GetClassifiedAdResponse response = await Mediator
+            .Send(query, cancellationToken);
+
+        return Ok(response);
+    }
+}
