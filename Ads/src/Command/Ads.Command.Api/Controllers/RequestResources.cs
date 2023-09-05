c98487870e48ecfe5d47cc5a14d52f471f2a8225
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-05 14:37:19 -0700

    RequestResources #1
    Remove default values from RequestResources in Ads.Command.Api

--- Ads/src/Command/Ads.Command.Api/Controllers/RequestResources.cs
+++ Ads/src/Command/Ads.Command.Api/Controllers/RequestResources.cs
@@ -2,23 +2,23 @@ namespace Ads.Command.Api.Controllers;
 
 public class CreateClassifiedAdRequestResource
 {
-    public string Description { get; set; } = string.Empty;
-    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string? Title { get; set; }
 }
 
 public class PublishClassifiedAdRequestResource
 {
-    public long ExpectedVersion { get; set; } = -1;
+    public long ExpectedVersion { get; set; }
 }
 
 public class UnpublishClassifiedAdRequestResource
 {
-    public long ExpectedVersion { get; set; } = -1;
+    public long ExpectedVersion { get; set; }
 }
 
 public class UpdateClassifiedAdRequestResource
 {
-    public string Description { get; set; } = string.Empty;
-    public long ExpectedVersion { get; set; } = -1;
-    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public long ExpectedVersion { get; set; }
+    public string? Title { get; set; }
 }
