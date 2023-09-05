51134bf7a56d1212dce6e2ed5e554138919a4abf
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-04 20:30:10 -0700

    ConfigureServices #1
    ConfigureServices in Ads.Query.Api

---
+++ Ads/src/Query/Ads.Query.Api/ConfigureServices.cs
@@ -0,0 +1,36 @@
+using Ads.Query.Api.Filters;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddRouting(options => options.LowercaseUrls = true);
+
+        services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication AddWebApplicationBuilder(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
