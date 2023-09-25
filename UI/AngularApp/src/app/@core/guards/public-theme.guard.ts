dd7315b673a416e0e12752fcc8e1501082cd267a
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-24 19:16:06 -0700

    public-theme.guard.ts #4
    Guard to allow access route for no authenticated users

---
+++ UI/AngularApp/src/app/@core/guards/public-theme.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from "@core/services/auth.service";
+
+export const publicThemetGuard: CanActivateFn = (
+    route: ActivatedRouteSnapshot, state: RouterStateSnapshot
+) => {
+    const authService: AuthService = inject(AuthService);
+    const router: Router = inject(Router);
+
+    if (!authService.isAuthenticated())
+        return true;
+
+    router.navigate(['app']);
+
+    return false;
+};
