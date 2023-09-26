28370f0a05e8241b6627398bc30ff30be33c3844
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-25 22:28:10 -0700

    AppThemeHeaderComponent #4

---
+++ UI/AngularApp/src/app/@theme/app-theme/components/app-theme-header/app-theme-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-theme-header',
+  templateUrl: './app-theme-header.component.html',
+  styleUrls: ['./app-theme-header.component.css']
+})
+export class AppThemeHeaderComponent {
+
+}
