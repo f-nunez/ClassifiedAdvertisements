c705015e358a20fdf4fb6a6754b98c60e3f3add1
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-25 22:28:45 -0700

    AppThemeSidebarComponent #4

---
+++ UI/AngularApp/src/app/@theme/app-theme/components/app-theme-sidebar/app-theme-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-theme-sidebar',
+  templateUrl: './app-theme-sidebar.component.html',
+  styleUrls: ['./app-theme-sidebar.component.css']
+})
+export class AppThemeSidebarComponent {
+
+}
