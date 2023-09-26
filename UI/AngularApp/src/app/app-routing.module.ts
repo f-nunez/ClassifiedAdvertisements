fb83b9ea4f4c0857abf2c9ad52b683d81e780593
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-25 22:34:10 -0700

    AppRoutingModule #4
    Add routes with lazy loading into AppRoutingModule #4

--- UI/AngularApp/src/app/app-routing.module.ts
+++ UI/AngularApp/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppThemeComponent } from '@theme/app-theme/app-theme.component';
+import { PublicThemeComponent } from '@theme/public-theme/public-theme.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicThemeComponent,
+    loadChildren: () => import('@theme/public-theme/public-theme.module').then(module => module.PublicThemeModule)
+  },
+  {
+    path: 'app',
+    component: AppThemeComponent,
+    loadChildren: () => import('@theme/app-theme/app-theme.module').then(module => module.AppThemeModule)
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
