1ca1720c6d06a11e1fe0edc13c194689b756dfd2
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:24:47 -0700

    ComponentsModule #4
    Import RouterModule into ComponentsModule for PublicThemeComponent

--- UI/AngularApp/src/app/@theme/public-theme/components/components.module.ts
+++ UI/AngularApp/src/app/@theme/public-theme/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicThemeHeaderComponent } from './public-theme-header/public-theme-header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     PublicThemeHeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     PublicThemeHeaderComponent
