e53fc2dbcc33a67c0aa02507d7bc2f9719533121
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:25:56 -0700

    PublicThemeComponent #4
    Handle signin into PublicThemeComponent

--- UI/AngularApp/src/app/@theme/public-theme/public-theme.component.ts
+++ UI/AngularApp/src/app/@theme/public-theme/public-theme.component.ts
@@ -1,4 +1,6 @@
 import { AfterContentInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 import { ScriptService } from '@core/services/script.service';
 
 @Component({
@@ -7,10 +9,14 @@ import { ScriptService } from '@core/services/script.service';
   styleUrls: ['./public-theme.component.css']
 })
 export class PublicThemeComponent implements AfterContentInit {
+  constructor(private authService: AuthService, private router: Router, private scriptService: ScriptService) { }
 
-  constructor(private scriptService: ScriptService) {
-  }
   ngAfterContentInit(): void {
     this.scriptService.loadScripts('theme.js');
+  }
+
+  async onSigninAsync() {
+    await this.authService.signinAsync();
+    this.router.navigate(['app']);
   }
 }
