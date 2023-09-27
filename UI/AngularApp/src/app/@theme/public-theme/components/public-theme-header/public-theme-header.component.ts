1d5eef0fd908d09cbd269dc276cf29387be61748
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:23:33 -0700

    PublicThemeHeaderComponent #4
    Signin event emitter into PublicThemeHeaderComponent

--- UI/AngularApp/src/app/@theme/public-theme/components/public-theme-header/public-theme-header.component.ts
+++ UI/AngularApp/src/app/@theme/public-theme/components/public-theme-header/public-theme-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-public-theme-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./public-theme-header.component.css']
 })
 export class PublicThemeHeaderComponent {
+  @Output() signin = new EventEmitter<void>();
 
+  onSignin() {
+    this.signin.emit();
+  }
 }
