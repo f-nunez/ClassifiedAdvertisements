3368597c0992cacc30ae4d332d509c7838e4ef50
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:28:18 -0700

    AppThemeHeaderComponent #4
    Signout event emitter

--- UI/AngularApp/src/app/@theme/app-theme/components/app-theme-header/app-theme-header.component.ts
+++ UI/AngularApp/src/app/@theme/app-theme/components/app-theme-header/app-theme-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-app-theme-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-theme-header.component.css']
 })
 export class AppThemeHeaderComponent {
+  @Output() signout = new EventEmitter<void>();
 
+  onSignout() {
+    this.signout.emit();
+  }
 }
