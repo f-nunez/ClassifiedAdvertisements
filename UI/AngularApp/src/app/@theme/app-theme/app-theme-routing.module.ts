5e971e5d95712dc3c695c792aaa51227d70cc66a
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:32:50 -0700

    AppThemeRoutingModule #4
    Add feature modules

--- UI/AngularApp/src/app/@theme/app-theme/app-theme-routing.module.ts
+++ UI/AngularApp/src/app/@theme/app-theme/app-theme-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Role } from '@core/enums/role';
+import { roleGuard } from '@core/guards/role.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
-    loadChildren: () => import('../../modules/home/home.module').then(module => module.HomeModule)
+    loadChildren: () => import('../../modules/home/home.module').then(module => module.HomeModule),
+    canActivate: [roleGuard],
+    data: { roles: [Role.Customer, Role.Manager, Role.Staff] }
+  },
+  {
+    path: 'my-ads',
+    loadChildren: () => import('../../modules/my-ads/my-ads.module').then(module => module.MyAdsModule),
+    canActivate: [roleGuard],
+    data: { roles: [Role.Customer, Role.Manager, Role.Staff] }
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('../../modules/users/users.module').then(module => module.UsersModule),
+    canActivate: [roleGuard],
+    data: { roles: [Role.Staff] }
   },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
