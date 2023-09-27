718d667037bf2a58f139ec0e351c338a40dba9b7
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-26 23:27:26 -0700

    ComponentsModule #4
    Import RouterModule and SharedModule

--- UI/AngularApp/src/app/@theme/app-theme/components/components.module.ts
+++ UI/AngularApp/src/app/@theme/app-theme/components/components.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppThemeHeaderComponent } from './app-theme-header/app-theme-header.component';
 import { AppThemeSidebarComponent } from './app-theme-sidebar/app-theme-sidebar.component';
+import { SharedModule } from '@shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,9 @@ import { AppThemeSidebarComponent } from './app-theme-sidebar/app-theme-sidebar.
     AppThemeSidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule
   ],
   exports: [
     AppThemeHeaderComponent,
