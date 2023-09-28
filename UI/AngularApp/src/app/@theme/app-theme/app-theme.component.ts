1c9cac84d27673b65a8600088f2510a205eab46f
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-27 19:39:18 -0700

    AppThemeComponent #9
    Project AppThemeColorDropdownComponent into AppThemeComponent

--- UI/AngularApp/src/app/@theme/app-theme/app-theme.component.ts
+++ UI/AngularApp/src/app/@theme/app-theme/app-theme.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
+import { ThemeColorDropdownItem } from '@theme/models/theme-color-dropdown-item';
+import { ThemeColorService } from '@theme/services/theme-color.service';
 import { ThemeToggleService } from '@theme/services/theme-toggle.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-app-theme',
@@ -9,16 +12,25 @@ import { ThemeToggleService } from '@theme/services/theme-toggle.service';
   styleUrls: ['./app-theme.component.css']
 })
 export class AppThemeComponent implements OnInit {
+  selectedThemeColorDropdownItem$: Observable<ThemeColorDropdownItem>;
+  themeColorDropdownItems$: Observable<ThemeColorDropdownItem[]>;
 
   constructor(
     private authService: AuthService,
     private router: Router,
+    private themeColorService: ThemeColorService,
     private themeToggleService: ThemeToggleService
   ) {
+    this.selectedThemeColorDropdownItem$ = this.themeColorService.getSelectedDropdownItemObservable();
+    this.themeColorDropdownItems$ = this.themeColorService.getDropdownItemsObservable();
   }
 
   ngOnInit(): void {
     this.themeToggleService.initializeToggle();
+  }
+
+  onSelectColor(item: ThemeColorDropdownItem) {
+    this.themeColorService.setSelectedColor(item);
   }
 
   onToggle() {
