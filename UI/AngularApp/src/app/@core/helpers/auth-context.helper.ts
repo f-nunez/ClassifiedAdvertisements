d0443518ad99952ca187c57c1147e87cc156223c
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-28 20:12:58 -0700

    AuthContextHelper #11
    Mapper for AuthContext from encoded jwt access token

---
+++ UI/AngularApp/src/app/@core/helpers/auth-context.helper.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Role } from '@core/enums/role';
+import { AuthContext } from '@core/models/auth-context';
+import { Claim } from '@core/models/claim';
+import { UserInfo } from '@core/models/user-info';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthContextHelper {
+    private jwtSeparator: string = '.';
+    private jwtPayloadPosition: number = 1;
+
+    public getAuthContext(accessToken: string): AuthContext {
+        let jwtPayload = this.getJwtPayload(accessToken);
+
+        let claims: Claim[] = jwtPayload;
+        let email: string = jwtPayload['email'];
+        let id: string = jwtPayload['sub'];
+        let name: string = jwtPayload['name'];
+        let roles: Role[] = jwtPayload['role'];
+
+        if (!Array.isArray(roles))
+            roles = [roles];
+
+        let username: string = jwtPayload['preferred_username'];
+
+        let userInfo: UserInfo = {
+            email: email,
+            id: id,
+            name: name,
+            roles: roles,
+            username: username
+        };
+
+        let authContext: AuthContext = {
+            claims: claims,
+            userInfo: userInfo
+        };
+
+        return authContext;
+    }
+
+    private getJwtPayload(accessToken: string): any {
+        let partsOfAccessToken = accessToken.split(this.jwtSeparator);
+        let encodedPayload = partsOfAccessToken[this.jwtPayloadPosition];
+        let decodedJwtPayload = window.atob(encodedPayload);
+        let jwtPayload = JSON.parse(decodedJwtPayload);
+
+        return jwtPayload;
+    }
+}
