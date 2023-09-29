d5a73981eb423d993f899b545be54c3f59cc44e7
Francisco Nuñez <fonfonferi@gmail.com>
2023-09-29 14:06:28 -0700

    Home module #13

--- UI/AngularApp/src/app/modules/home/home-routing.module.ts
+++ UI/AngularApp/src/app/modules/home/home-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './containers/home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent }
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forChild(routes)
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class HomeRoutingModule { }
--- UI/AngularApp/src/app/modules/home/home.module.ts
+++ UI/AngularApp/src/app/modules/home/home.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { SharedModule } from '@shared/shared.module';
+import { HomeContainersModule } from './containers/home-containers.module';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
+    SharedModule,
+    HomeContainersModule,
     HomeRoutingModule
-  ],
-  exports: [
-    HomeComponent
   ]
 })
 export class HomeModule { }
