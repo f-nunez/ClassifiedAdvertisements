96e43b32ba0598ef6d86e36db109a0ae03781bdb
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-02 22:45:55 -0700

    MyAdsService #15
    Mocking returned data from api

--- UI/AngularApp/src/app/modules/my-ads/services/my-ads.service.ts
+++ UI/AngularApp/src/app/modules/my-ads/services/my-ads.service.ts
@@ -13,22 +13,29 @@ export class MyAdsService {
         // let params = new HttpParams();
         // params = params.append('skip', request.skip);
         // params = params.append('take', request.take);
-        // items = httpClient.get<GetMyAdsListResponse>('apiQueryUrl' + 'myads', { params });
+        // response = httpClient.get<GetMyAdsListResponse>('apiQueryUrl' + 'myads', { params });
 
-        for (let i = request.skip; i < request.skip + request.take; i++) {
+        for (let i = 0; i < request.take; i++) {
             let dummyItem: GetMyAdsListItem = {
                 description: `The dummy description ${i}`,
                 id: `${i}`,
                 publishedOn: Date.now().toString(),
-                title: `The Dummy title ${i}`,
+                title: `The Dummy title ${i} from page ${this.getSelectedPage(request.skip, request.take)}`,
                 updatedOn: Date.now().toString()
             };
 
             items.push(dummyItem);
         }
 
-        let response: GetMyAdsListResponse = { count: 10, items: items };
+        let response: GetMyAdsListResponse = { count: 110, items: items };
 
         return of(response);
+    }
+
+    getSelectedPage(skip: number, take: number) {
+        let selectedPageNumber = Math.ceil((skip + 1) / take);
+        selectedPageNumber = selectedPageNumber > 0 ? selectedPageNumber : 1;
+
+        return selectedPageNumber;
     }
 }
