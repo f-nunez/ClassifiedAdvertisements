1f008a27999e0e8cef7d90851bb1bd041d924ce5
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-03 15:17:57 -0700

    MyAdsRoutingModule #15
    Add route path for MyAdsCreateComponent

--- UI/AngularApp/src/app/modules/my-ads/my-ads-routing.module.ts
+++ UI/AngularApp/src/app/modules/my-ads/my-ads-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyAdsListComponent } from './containers/my-ads-list/my-ads-list.component';
+import { MyAdsCreateComponent } from './containers/my-ads-create/my-ads-create.component';
 
-const routes: Routes = [{ path: '', component: MyAdsListComponent }];
+const routes: Routes = [
+  { path: '', component: MyAdsListComponent },
+  { path: 'create', component: MyAdsCreateComponent }
+];
 
 @NgModule({
   declarations: [],
