0736262a18260fecd6174d5312cd0c105d8c675e
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-04 19:38:28 -0700

    SharedComponentsModule #15
    Import components

--- UI/AngularApp/src/app/@shared/components/shared-components.module.ts
+++ UI/AngularApp/src/app/@shared/components/shared-components.module.ts
@@ -1,15 +1,21 @@
 import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TablePaginatorComponent } from './table-paginator/table-paginator.component';
-import { FormsModule } from '@angular/forms';
+import { FormInputComponent } from './form-input/form-input.component';
+import { FormLabelComponent } from './form-label/form-label.component';
+import { FormTextareaComponent } from './form-textarea/form-textarea.component';
 
 export const components: Type<any>[] = [
+  FormInputComponent,
+  FormLabelComponent,
+  FormTextareaComponent,
   TablePaginatorComponent
 ];
 
 @NgModule({
   declarations: [...components],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [...components]
 })
 export class SharedComponentsModule { }
