b67ba3d2833401d9f46c3137cd01567d7e86aa8b
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-04 19:39:32 -0700

    MyAdsCreateComponent #15
    Styling container component

--- UI/AngularApp/src/app/modules/my-ads/containers/my-ads-create/my-ads-create.component.ts
+++ UI/AngularApp/src/app/modules/my-ads/containers/my-ads-create/my-ads-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-ads-create',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-ads-create.component.css']
 })
 export class MyAdsCreateComponent {
+  validated: boolean = false;
 
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  createAdForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required]
+  });
+
+  onClickBackToList(): void {
+    this.backToList();
+  }
+
+  onSubmitSave(): void {
+    this.validated = true;
+
+    if (this.createAdForm.invalid)
+      return;
+
+    alert('save it!');
+  }
+
+  private backToList(): void {
+    this.router.navigate(['app/my-ads']);
+  }
 }
