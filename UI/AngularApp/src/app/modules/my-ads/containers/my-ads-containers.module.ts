38a2f04d127c8f96fff75d06208fc68379baded3
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-06 13:38:06 -0700

    MyAdsContainers #15
    Import MyAdsUpdateComponent

--- UI/AngularApp/src/app/modules/my-ads/containers/my-ads-containers.module.ts
+++ UI/AngularApp/src/app/modules/my-ads/containers/my-ads-containers.module.ts
@@ -3,10 +3,12 @@ import { SharedModule } from '@shared/shared.module';
 import { MyAdsComponentsModule } from '../components/my-ads-components.module';
 import { MyAdsCreateComponent } from './my-ads-create/my-ads-create.component';
 import { MyAdsListComponent } from './my-ads-list/my-ads-list.component';
+import { MyAdsUpdateComponent } from './my-ads-update/my-ads-update.component';
 
 const containers: Array<Type<any>> = [
   MyAdsCreateComponent,
-  MyAdsListComponent
+  MyAdsListComponent,
+  MyAdsUpdateComponent
 ];
 
 @NgModule({
