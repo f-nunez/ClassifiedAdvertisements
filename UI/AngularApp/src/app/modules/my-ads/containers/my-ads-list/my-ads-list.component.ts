7b3950dca0f7c00f6d4b3e87c797cb116c90edcf
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-06 13:40:26 -0700

    MyAdsListComponent #15
    Add event onClickEdit

--- UI/AngularApp/src/app/modules/my-ads/containers/my-ads-list/my-ads-list.component.ts
+++ UI/AngularApp/src/app/modules/my-ads/containers/my-ads-list/my-ads-list.component.ts
@@ -3,7 +3,7 @@ import { GetMyAdsListItem } from '../../interfaces/my-ads-list/get-my-ads-list-i
 import { MyAdsService } from '../../services/my-ads.service';
 import { GetMyAdsListRequest } from '../../interfaces/my-ads-list/get-my-ads-list-request';
 import { TablePaginatorEvent } from '@shared/components/table-paginator/table-paginator-event';
-import { Route, Router, Routes } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-ads-list',
@@ -23,6 +23,10 @@ export class MyAdsListComponent {
 
   onClickAdd(): void {
     this.router.navigate(['app/my-ads/create']);
+  }
+
+  onClickEdit(id: string): void {
+    this.router.navigate(['app/my-ads/update', id]);
   }
 
   onPaginationEvent(trigger: TablePaginatorEvent): void {
