91c04d82c18c6c386976e3541187d11893888936
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-09 22:00:54 -0700

    SharedComponentsModule #15
    Delete TablePaginatorComponent

--- UI/AngularApp/src/app/@shared/components/shared-components.module.ts
+++ UI/AngularApp/src/app/@shared/components/shared-components.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TablePaginatorComponent } from './table-paginator/table-paginator.component';
 import { FormInputComponent } from './form-input/form-input.component';
 import { FormLabelComponent } from './form-label/form-label.component';
 import { FormTextareaComponent } from './form-textarea/form-textarea.component';
@@ -9,8 +8,7 @@ import { FormTextareaComponent } from './form-textarea/form-textarea.component';
 export const components: Type<any>[] = [
   FormInputComponent,
   FormLabelComponent,
-  FormTextareaComponent,
-  TablePaginatorComponent
+  FormTextareaComponent
 ];
 
 @NgModule({
