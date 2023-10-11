031e7fef1bcaf680fdb254f2fd5ac6e3feac68bb
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-10 20:33:40 -0700

    GetMyAdListResponse #17
    Refactor according to action module

---
+++ UI/AngularApp/src/app/modules/my-ads/interfaces/get-my-ad-list/get-my-ad-list-response.ts
@@ -0,0 +1,6 @@
+import { GetMyAdListItem } from './get-my-ad-list-item';
+
+export interface GetMyAdListResponse {
+    count: number,
+    items: GetMyAdListItem[]
+}
--- UI/AngularApp/src/app/modules/my-ads/interfaces/get-my-ads-list/get-my-ads-list-response.ts
+++
@@ -1,6 +0,0 @@
-import { GetMyAdsListItem } from './get-my-ads-list-item';
-
-export interface GetMyAdsListResponse {
-    count: number,
-    items: GetMyAdsListItem[]
-}
