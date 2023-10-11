4c97c2314f9bd6c9749df7b7ac259f92bb1a78e2
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-10 20:36:10 -0700

    MyAdsListComponent #17
    Refactor according to renaming interfaces

--- UI/AngularApp/src/app/modules/my-ads/containers/my-ads-list/my-ads-list.component.ts
+++ UI/AngularApp/src/app/modules/my-ads/containers/my-ads-list/my-ads-list.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataTableSetting } from '@shared/modules/data-table';
 import { DeleteMyAdRequest } from '../../interfaces/delete-my-ad/delete-my-ad-request';
-import { GetMyAdsListItem } from '../../interfaces/get-my-ads-list/get-my-ads-list-item';
-import { GetMyAdsListRequest } from '../../interfaces/get-my-ads-list/get-my-ads-list-request';
+import { GetMyAdListItem } from '../../interfaces/get-my-ad-list/get-my-ad-list-item';
+import { GetMyAdListRequest } from '../../interfaces/get-my-ad-list/get-my-ad-list-request';
 import { MyAdsService } from '../../services/my-ads.service';
 
 @Component({
@@ -13,7 +13,7 @@ import { MyAdsService } from '../../services/my-ads.service';
 })
 export class MyAdsListComponent {
   count: number = 0;
-  items: GetMyAdsListItem[] = [];
+  items: GetMyAdListItem[] = [];
   skip: number = 0;
   take: number = 10;
   sortColumnPropertyName: string = '';
@@ -32,7 +32,7 @@ export class MyAdsListComponent {
     this.take = value.take;
     this.sortColumnPropertyName = value.sortColumnPropertyName;
     this.sortIsAscending = value.sortIsAscending;
-    this.getMyAdsList();
+    this.loadData();
   }
 
   onClickAdd(): void {
@@ -55,7 +55,7 @@ export class MyAdsListComponent {
     let request: DeleteMyAdRequest = { id: id };
     this.myAdsService.deleteMyAd(request).subscribe({
       next: (response) => {
-        this.getMyAdsList();
+        this.loadData();
       },
       error: (error) => { console.log(error); }
     });
@@ -76,8 +76,8 @@ export class MyAdsListComponent {
     return this.dataTableSetting;
   }
 
-  private getMyAdsList(): void {
-    let request: GetMyAdsListRequest = {
+  private loadData(): void {
+    let request: GetMyAdListRequest = {
       skip: this.skip,
       take: this.take,
       sortColumn: this.sortColumnPropertyName,
