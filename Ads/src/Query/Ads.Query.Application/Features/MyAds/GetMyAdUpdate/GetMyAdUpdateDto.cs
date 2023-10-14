b0dcf5c6cf3a7e605f36539c0145a43c87acc970
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-14 13:23:46 -0700

    GetMyAdUpdateDto #17
    GetMyAdUpdateDto in Ads.Query.Application

---
+++ Ads/src/Query/Ads.Query.Application/Features/MyAds/GetMyAdUpdate/GetMyAdUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace Ads.Query.Application.Features.MyAds.GetMyAdUpdate;
+
+public class GetMyAdUpdateDto
+{
+    public string? Description { get; set; }
+    public string? Id { get; set; }
+    public string? Title { get; set; }
+    public long Version { get; set; }
+}
