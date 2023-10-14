8f6881311a364c0211f4eab71fdee91a50eb7cd5
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-13 20:47:08 -0700

    Removing Ads.Command feature module #17

--- Ads/src/Command/Ads.Command.Application/Features/PublishClassifiedAd/PublishClassifiedAdCommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace Ads.Command.Application.Features.PublishClassifiedAd;
-
-public record PublishClassifiedAdCommand(PublishClassifiedAdRequest PublishClassifiedAdRequest)
-    : IRequest<PublishClassifiedAdResponse>;
--- Ads/src/Command/Ads.Command.Application/Features/PublishClassifiedAd/PublishClassifiedAdRequest.cs
+++
@@ -1,9 +0,0 @@
-using Ads.Command.Application.Common.Requests;
-
-namespace Ads.Command.Application.Features.PublishClassifiedAd;
-
-public class PublishClassifiedAdRequest : BaseRequest
-{
-    public string? ClassifiedAdId { get; set; }
-    public long ExpectedVersion { get; set; }
-}
