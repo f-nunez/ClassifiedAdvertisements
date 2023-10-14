37aa9f11df98154460462864c6cb1ed3d6511e18
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-13 21:31:19 -0700

    DataTableRequest #17

---
+++ Ads/src/Query/Ads.Query.Application/Common/Requests/DataTableRequest.cs
@@ -0,0 +1,8 @@
+namespace Ads.Query.Application.Common.Requests;
+
+public class DataTableRequest
+{
+    public int Skip { get; set; }
+    public List<DataTableRequestSort>? Sorts { get; set; }
+    public int Take { get; set; }
+}
