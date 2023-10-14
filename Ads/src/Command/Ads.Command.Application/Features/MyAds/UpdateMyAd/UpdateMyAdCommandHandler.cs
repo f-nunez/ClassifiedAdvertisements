792ff3286dc18489bb94430217792f65d0fcb11a
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-14 12:24:16 -0700

    UpdateMyAdCommandHandler #17
    UpdateMyAdCommandHandler in Ads.Command.Application

---
+++ Ads/src/Command/Ads.Command.Application/Features/MyAds/UpdateMyAd/UpdateMyAdCommandHandler.cs
@@ -0,0 +1,51 @@
+using Ads.Command.Application.Common.Exceptions;
+using Ads.Command.Application.Common.Interfaces;
+using Ads.Command.Domain.ClassifiedAdAggregate;
+using MediatR;
+
+namespace Ads.Command.Application.Features.UpdateMyAd;
+
+public class UpdateMyAdCommandHandler
+    : IRequestHandler<UpdateMyAdCommand, UpdateMyAdResponse>
+{
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IEventStore<ClassifiedAd> _eventStore;
+
+    public UpdateMyAdCommandHandler(
+        ICurrentUserService currentUserService,
+        IEventStore<ClassifiedAd> eventStore)
+    {
+        _currentUserService = currentUserService;
+        _eventStore = eventStore;
+    }
+
+    public async Task<UpdateMyAdResponse> Handle(
+        UpdateMyAdCommand command,
+        CancellationToken cancellationToken)
+    {
+        UpdateMyAdRequest request = command.UpdateMyAdRequest;
+
+        var response = new UpdateMyAdResponse(request.CorrelationId);
+
+        var classifiedAd = await _eventStore.ReadStreamEventsAsync(request.Id);
+
+        if (classifiedAd is null)
+            throw new NotFoundException(nameof(classifiedAd), request.Id);
+
+        classifiedAd.Update(
+            request.Description,
+            request.Title,
+           _currentUserService.UserId,
+            DateTimeOffset.UtcNow
+        );
+
+        await _eventStore.AppendEventsAsync(
+            classifiedAd,
+            request.Version,
+            request.CorrelationId,
+            cancellationToken
+        );
+
+        return response;
+    }
+}
