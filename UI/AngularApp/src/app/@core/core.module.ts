fd0fa02a3c5bca4424fa718f76be78da7bb55628
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-14 22:43:04 -0700

    CoreModule #21
    add HttpErrorInterceptor as provider into CoreModule

--- UI/AngularApp/src/app/@core/core.module.ts
+++ UI/AngularApp/src/app/@core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 
 @NgModule({
   declarations: [],
@@ -10,6 +11,13 @@ import { HttpClientModule } from "@angular/common/http";
   ],
   exports: [
     HttpClientModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
