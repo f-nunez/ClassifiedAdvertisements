7cb17b38c034701b7626038bda34a0ca68a28740
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-15 17:47:19 -0700

    Repository #23
    Catch StreamNotFoundException into Repository

--- Ads/src/Command/Ads.Command.Infrastructure/Persistence/Repositories/IRepository.cs
+++ Ads/src/Command/Ads.Command.Infrastructure/Persistence/Repositories/IRepository.cs
@@ -5,5 +5,5 @@ namespace Ads.Command.Infrastructure.Persistence.Repositories;
 public interface IRepository
 {
     Task AppendEventAsync(string streamName, EventData eventData, long expectedVersion, CancellationToken cancellationToken = default);
-    Task<List<ResolvedEvent>> ReadStreamEventsAsync(string streamName, CancellationToken cancellationToken = default);
+    Task<List<ResolvedEvent>?> ReadStreamEventsAsync(string streamName, CancellationToken cancellationToken = default);
 }
