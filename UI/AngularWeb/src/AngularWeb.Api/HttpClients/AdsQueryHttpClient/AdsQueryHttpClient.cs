f497ea7a7fbb6ae5583a08bcca380877e1748548
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-24 22:57:57 -0700

    AdsQueryHttpClient #25
    Implement BaseHttpClient

--- UI/AngularWeb/src/AngularWeb.Api/HttpClients/AdsQueryHttpClient/AdsQueryHttpClient.cs
+++ UI/AngularWeb/src/AngularWeb.Api/HttpClients/AdsQueryHttpClient/AdsQueryHttpClient.cs
@@ -1,70 +1,58 @@
-using System.Text.Json;
 using AngularWeb.Api.Application.Features.MyAds.Queries.GetMyAdDetail;
 using AngularWeb.Api.Application.Features.MyAds.Queries.GetMyAdList;
 using AngularWeb.Api.Application.Features.MyAds.Queries.GetMyAdUpdate;
 
 namespace AngularWeb.Api.HttpClients;
 
-public class AdsQueryHttpClient : IAdsQueryHttpClient
+public class AdsQueryHttpClient : BaseHttpClient, IAdsQueryHttpClient
 {
-    private static readonly JsonSerializerOptions s_serializerOptions = new(JsonSerializerDefaults.Web);
-    private readonly HttpClient _httpClient;
-
     public AdsQueryHttpClient(HttpClient httpClient)
+        : base(httpClient)
     {
-        if (httpClient is null)
-            throw new ArgumentNullException(nameof(httpClient));
-
-        _httpClient = httpClient;
     }
 
-    public async Task<GetMyAdDetailResponse> GetMyAdDetailAsync(string id, CancellationToken cancellationToken)
+    public async Task<GetMyAdDetailResponse> GetMyAdDetailAsync(
+        string id,
+        CancellationToken cancellationToken)
     {
-        var uri = $"v1/myads/{id}/detail";
-        var request = CreateRequest(HttpMethod.Get, uri);
-        using var result = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-        using var contentStream = await result.Content.ReadAsStreamAsync(cancellationToken);
-        var response = await JsonSerializer.DeserializeAsync<GetMyAdDetailResponse>(
-            contentStream,
-            s_serializerOptions,
+        var relativeUri = $"v1/myads/{id}/detail";
+
+        var response = await HttpGetAsync<GetMyAdDetailResponse>(
+            relativeUri,
             cancellationToken
         );
 
         return response ?? throw new ArgumentNullException(nameof(response));
     }
 
-    public async Task<GetMyAdListResponse> GetMyAdListAsync(int skip, int take, bool sortAsc, string sortProp, CancellationToken cancellationToken)
+    public async Task<GetMyAdListResponse> GetMyAdListAsync(
+        int skip,
+        int take,
+        bool sortAsc,
+        string sortProp,
+        CancellationToken cancellationToken)
     {
-        string uri = $"v1/myads?skip={skip}&take={take}&sortasc={sortAsc}&sortprop={sortProp}";
-        var request = CreateRequest(HttpMethod.Get, uri);
-        using var result = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-        using var contentStream = await result.Content.ReadAsStreamAsync(cancellationToken);
-        var response = await JsonSerializer.DeserializeAsync<GetMyAdListResponse>(
-            contentStream,
-            s_serializerOptions,
+        string relativeUri = $"v1/myads?skip={skip}&take={take}&sortasc={sortAsc}&sortprop={sortProp}";
+
+        var response = await HttpGetAsync<GetMyAdListResponse>(
+            relativeUri,
             cancellationToken
         );
 
         return response ?? throw new ArgumentNullException(nameof(response));
     }
 
-    public async Task<GetMyAdUpdateResponse> GetMyAdUpdateAsync(string id, CancellationToken cancellationToken)
+    public async Task<GetMyAdUpdateResponse> GetMyAdUpdateAsync(
+        string id,
+        CancellationToken cancellationToken)
     {
-        var uri = $"v1/myads/{id}/update";
-        var request = CreateRequest(HttpMethod.Get, uri);
-        using var result = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-        using var contentStream = await result.Content.ReadAsStreamAsync(cancellationToken);
-        var response = await JsonSerializer.DeserializeAsync<GetMyAdUpdateResponse>(
-            contentStream,
-            s_serializerOptions,
+        var relativeUri = $"v1/myads/{id}/update";
+
+        var response = await HttpGetAsync<GetMyAdUpdateResponse>(
+            relativeUri,
             cancellationToken
         );
 
         return response ?? throw new ArgumentNullException(nameof(response));
-    }
-
-    private static HttpRequestMessage CreateRequest(HttpMethod httpMethod, string? requestUri)
-    {
-        return new HttpRequestMessage(httpMethod, requestUri);
     }
 }
