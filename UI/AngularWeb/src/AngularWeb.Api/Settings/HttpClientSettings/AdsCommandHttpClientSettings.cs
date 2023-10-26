5bacdb0fe4f5c38c8bf408e4001be3c91a759461
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-25 19:12:47 -0700

    AdsCommandHttpClientSettings #25
    Implement BaseHttpClientSettings

--- UI/AngularWeb/src/AngularWeb.Api/Settings/HttpClientSettings/AdsCommandHttpClientSettings.cs
+++ UI/AngularWeb/src/AngularWeb.Api/Settings/HttpClientSettings/AdsCommandHttpClientSettings.cs
@@ -1,8 +1,5 @@
 namespace AngularWeb.Api.Settings;
 
-public class AdsCommandHttpClientSettings
+public class AdsCommandHttpClientSettings : BaseHttpClientSettings
 {
-    public required Uri BaseAddress { get; set; }
-    public int HandlerLifetimeInSeconds { get; set; }
-    public int TimeoutInSeconds { get; set; }
 }
