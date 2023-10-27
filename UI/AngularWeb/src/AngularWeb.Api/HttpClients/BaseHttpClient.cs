44a0b598135fc64aa5e47cdca49bd40b4620b70f
Francisco Nuñez <fonfonferi@gmail.com>
2023-10-26 19:19:55 -0700

    BaseHttpClient #25
    Validate HttpResponseMessage to catch problem details

--- UI/AngularWeb/src/AngularWeb.Api/HttpClients/BaseHttpClient.cs
+++ UI/AngularWeb/src/AngularWeb.Api/HttpClients/BaseHttpClient.cs
@@ -1,9 +1,12 @@
 using System.Text.Json;
+using AngularWeb.Exceptions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AngularWeb.Api.HttpClients;
 
 public abstract class BaseHttpClient
 {
+    private static readonly string s_problemDetailsMediaType = "application/problem+json";
     private static readonly JsonSerializerOptions s_serializerOptions = new(JsonSerializerDefaults.Web);
     private readonly HttpClient _httpClient;
 
@@ -24,11 +27,13 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Delete, requestUri);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
+
+        await ValidateResponseAsync(response, cancellationToken);
     }
 
     protected async Task<T?> HttpDeleteAsync<T>(
@@ -37,13 +42,15 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Delete, requestUri);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
 
-        using var contentStream = await result.Content
+        await ValidateResponseAsync(response, cancellationToken);
+
+        using var contentStream = await response.Content
             .ReadAsStreamAsync(cancellationToken);
 
         return await DeserializeAsync<T>(contentStream, cancellationToken);
@@ -55,13 +62,15 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Get, requestUri);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
 
-        return await result.Content.ReadAsStringAsync(cancellationToken);
+        await ValidateResponseAsync(response, cancellationToken);
+
+        return await response.Content.ReadAsStringAsync(cancellationToken);
     }
 
     protected async Task<T?> HttpGetAsync<T>(
@@ -70,13 +79,15 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Get, requestUri);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
 
-        using var contentStream = await result.Content
+        await ValidateResponseAsync(response, cancellationToken);
+
+        using var contentStream = await response.Content
             .ReadAsStreamAsync(cancellationToken);
 
         return await DeserializeAsync<T>(contentStream, cancellationToken);
@@ -89,11 +100,13 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Post, requestUri, data);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
+
+        await ValidateResponseAsync(response, cancellationToken);
     }
 
     protected async Task<T?> HttpPostAsync<T>(
@@ -103,13 +116,15 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Post, requestUri, data);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
 
-        using var contentStream = await result.Content
+        await ValidateResponseAsync(response, cancellationToken);
+
+        using var contentStream = await response.Content
             .ReadAsStreamAsync(cancellationToken);
 
         return await DeserializeAsync<T>(contentStream, cancellationToken);
@@ -122,11 +137,13 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Put, requestUri, data);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
+
+        await ValidateResponseAsync(response, cancellationToken);
     }
 
     protected async Task<T?> HttpPutAsync<T>(
@@ -136,13 +153,15 @@ public abstract class BaseHttpClient
     {
         var request = CreateRequest(HttpMethod.Put, requestUri, data);
 
-        using var result = await _httpClient.SendAsync(
+        using var response = await _httpClient.SendAsync(
             request,
             HttpCompletionOption.ResponseHeadersRead,
             cancellationToken
         );
 
-        using var contentStream = await result.Content
+        await ValidateResponseAsync(response, cancellationToken);
+
+        using var contentStream = await response.Content
             .ReadAsStreamAsync(cancellationToken);
 
         return await DeserializeAsync<T>(contentStream, cancellationToken);
@@ -162,7 +181,7 @@ public abstract class BaseHttpClient
     {
         return new HttpRequestMessage(httpMethod, requestUri)
         {
-            Content = new JsonContent(Serialize(data))
+            Content = new JsonContent(JsonSerializer.Serialize(data))
         };
     }
 
@@ -176,8 +195,22 @@ public abstract class BaseHttpClient
         );
     }
 
-    private static string Serialize(object data)
+    private static async Task ValidateResponseAsync(
+        HttpResponseMessage response,
+        CancellationToken cancellationToken = default)
     {
-        return JsonSerializer.Serialize(data);
+        if (response.IsSuccessStatusCode)
+            return;
+
+        if (response.Content.Headers.ContentType?.MediaType == s_problemDetailsMediaType)
+        {
+            var problemDetails = JsonSerializer.Deserialize<ProblemDetails>(
+                await response.Content.ReadAsStreamAsync(cancellationToken));
+
+            if (problemDetails is not null)
+                throw new HttpResponseException(problemDetails);
+        }
+
+        response.EnsureSuccessStatusCode();
     }
 }
