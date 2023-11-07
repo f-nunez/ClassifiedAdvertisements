6f2cf9e323538ea70459072359e3c1aa8acd8ed6
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-07 15:24:32 -0800

    CoreModule #27
    Add JwtInterceptor into CoreModule for TokenBasedAuth approach

--- UI/AngularWeb/src/AngularWeb.Client/src/app/@core/core.module.ts
+++ UI/AngularWeb/src/AngularWeb.Client/src/app/@core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [],
@@ -16,6 +17,11 @@ import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
       multi: true
     }
   ]
