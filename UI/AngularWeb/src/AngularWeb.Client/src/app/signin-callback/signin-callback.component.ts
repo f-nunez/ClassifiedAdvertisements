67c49608e6b2253fdd7d258ec1489f060f4e4570
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-07 15:16:16 -0800

    SigningCallbackComponent #27
    Handling signing callback with subscription observer

--- UI/AngularWeb/src/AngularWeb.Client/src/app/signin-callback/signin-callback.component.ts
+++ UI/AngularWeb/src/AngularWeb.Client/src/app/signin-callback/signin-callback.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@core/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signin-callback',
   templateUrl: './signin-callback.component.html',
   styleUrls: ['./signin-callback.component.css']
 })
-export class SigninCallbackComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) { }
+export class SigninCallbackComponent implements OnInit, OnDestroy {
+  private subscription: Subscription | undefined;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   async ngOnInit() {
+    this.subscription = this.authService.getAuthenticatedObservable()
+      .subscribe((res) => {
+        if (res)
+          this.router.navigate(['']);
+      });
+
     await this.authService.signinCallbackAsync();
-    this.router.navigate(['']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
