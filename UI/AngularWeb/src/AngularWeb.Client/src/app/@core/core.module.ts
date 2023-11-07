bd6ce00256d5b074b1ccf75ac29fa819aff763bd
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-07 15:25:02 -0800

    CoreModule #27
    Add CsrfInterceptor into CoreModule for CookieBasedAuth approach

--- UI/AngularWeb/src/AngularWeb.Client/src/app/@core/core.module.ts
+++ UI/AngularWeb/src/AngularWeb.Client/src/app/@core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { CsrfHeaderInterceptor } from './interceptors/csrf-header.interceptor';
 
 @NgModule({
   declarations: [],
@@ -22,6 +23,11 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CsrfHeaderInterceptor,
       multi: true
     }
   ]
