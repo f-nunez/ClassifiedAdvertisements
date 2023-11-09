300331d12880d87aff1ac0fc13a1164ba197dd01
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-08 22:52:56 -0800

    AuthorizationSettings #27
    AuthorizationSettings in Ads.Command.Api

---
+++ Ads/src/Command/Ads.Command.Api/Settings/AuthorizationSettings.cs
@@ -0,0 +1,16 @@
+namespace Ads.Command.Api.Settings;
+
+public class AuthorizationSettings
+{
+    public required Policy[] Policies { get; set; }
+}
+
+public class Policy
+{
+    public required string Name { get; set; }
+    public bool RequireAuthenticatedUser { get; set; }
+    public RequiredClaim[]? RequiredClaims { get; set; }
+    public string[]? RequiredRoles { get; set; }
+}
+
+public record RequiredClaim(string ClaimType, string[] Values);
