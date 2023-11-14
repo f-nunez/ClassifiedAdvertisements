b2eda06d309fe07a3afb1d0dc5c1d7c28b22a9c8
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-13 19:09:19 -0800

    AngularWebTokenClientSettings

--- Identity/src/Identity.Infrastructure/Settings/AngularWebTokenClientSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Identity.Infrastructure.Settings;
-
-public class AngularWebTokenClientSettings
-{
-    public List<string> AllowedCorsOrigins { get; set; } = new();
-    public List<string> PostLogoutRedirectUris { get; set; } = new();
-    public List<string> RedirectUris { get; set; } = new();
-}
