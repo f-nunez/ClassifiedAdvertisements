b397af5bb3681c20234a6f4398d179e10fba988b
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-13 19:20:31 -0800

    Add Initial migrations in Ads.Command.Infrastructure #33

---
+++ Ads/src/Command/Ads.Command.Infrastructure/Persistence/Migrations/20231114025704_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ads.Command.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StreamStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EventPayload = table.Column<string>(type: "text", nullable: false),
+                    EventType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    StreamName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StreamStates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StreamStates_Version_StreamName",
+                table: "StreamStates",
+                columns: new[] { "Version", "StreamName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StreamStates");
+        }
+    }
+}
