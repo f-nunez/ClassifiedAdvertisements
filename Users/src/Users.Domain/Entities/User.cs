da42421987499de46aee0ff1aef1cc21edcdd862
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-19 22:26:19 -0800

    User #39

---
+++ Users/src/Users.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+using Users.Domain.Common;
+
+namespace Users.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string? FullName { get; set; }
+    public string? NormalizedFullName { get; set; }
+    public string? FirstName { get; set; }
+    public string? NormalizedFirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? NormalizedLastName { get; set; }
+    public string? Email { get; set; }
+    public string? NormalizedEmail { get; set; }
+    public string? UserName { get; set; }
+    public string? NormalizedUserName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? ProfileImageUrl { get; set; }
+}
