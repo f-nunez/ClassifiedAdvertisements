2f30e038e28b9431fcf65cd51672c94c4cac3446
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-20 20:07:40 -0800

    DataTableRequest #39

---
+++ Users/src/Users.Application/Common/Requests/DataTableRequest.cs
@@ -0,0 +1,8 @@
+namespace Users.Application.Common.Requests;
+
+public class DataTableRequest
+{
+    public int Skip { get; set; }
+    public List<DataTableRequestSort>? Sorts { get; set; }
+    public int Take { get; set; }
+}
