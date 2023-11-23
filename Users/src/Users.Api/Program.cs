3e8a8fb83972e6ab125a8a377d604d4ec568c9ef
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-22 20:34:46 -0800

    Program #39
    Adding dependencies services

--- Users/src/Users.Api/Program.cs
+++ Users/src/Users.Api/Program.cs
@@ -1,19 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddWebServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.AddWebApplicationBuilder();
 
 app.Run();
