207101782603f9c2a92637adb2b9406028241c22
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-24 19:55:03 -0800

    Roles #39
    Abstract class to define the existing roles in Users.Domain

---
+++ Users/src/Users.Domain/Constants/Roles.cs
@@ -0,0 +1,8 @@
+namespace Users.Domain.Constants;
+
+public abstract class Roles
+{
+    public const string Customer = nameof(Customer);
+    public const string Manager = nameof(Manager);
+    public const string Staff = nameof(Staff);
+}
