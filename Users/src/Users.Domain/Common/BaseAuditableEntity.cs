83c932d60c00592a36535a048a7090255f47af5a
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-24 19:53:24 -0800

    BaseAuditableEntity #39
    BaseAuditableEntity in Users.Domain

---
+++ Users/src/Users.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,9 @@
+namespace Users.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public string? CreatedBy { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTimeOffset? UpdatedOn { get; set; }
+}
