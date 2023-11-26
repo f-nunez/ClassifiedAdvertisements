5ca45d9fe0cd6e76e85c5987f5e07513c3d5736b
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-25 18:39:40 -0800

    Role #39
    Implement BaseAuditableEntity

--- Users/src/Users.Domain/Entities/Role.cs
+++ Users/src/Users.Domain/Entities/Role.cs
@@ -2,7 +2,7 @@ using Users.Domain.Common;
 
 namespace Users.Domain.Entities;
 
-public class Role : BaseEntity
+public class Role : BaseAuditableEntity
 {
     public string? Name { get; set; }
     public string? NormalizedName { get; set; }
