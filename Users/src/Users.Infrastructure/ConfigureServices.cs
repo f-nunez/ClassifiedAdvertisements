1208c92c22207d422e1986437de08352c21bb66e
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-28 23:47:49 -0800

    ConfigureServices #39
    AddMassTransitServiceBus method into ConfigureServices in Users.Infrastructure

--- Users/src/Users.Infrastructure/ConfigureServices.cs
+++ Users/src/Users.Infrastructure/ConfigureServices.cs
@@ -1,9 +1,13 @@
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Users.Application.Common.Interfaces;
 using Users.Domain.Entities;
 using Users.Infrastructure.Persistence.Contexts;
 using Users.Infrastructure.Persistence.Repositories;
+using Users.Infrastructure.ServiceBus;
+using Users.Infrastructure.ServiceBus.Observers;
+using Users.Infrastructure.Settings;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +19,8 @@ public static class ConfigureServices
     {
 
         services.AddDatabase(configuration);
+
+        services.AddMassTransitServiceBus(configuration);
 
         return services;
     }
@@ -39,6 +45,60 @@ public static class ConfigureServices
         services.AddScoped<IRepository<User>, Repository<User>>();
 
         services.AddScoped<IRepository<UserRole>, Repository<UserRole>>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddMassTransitServiceBus(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IServiceBus, MassTransitServiceBus>();
+
+        services.AddConsumeObserver<LoggingConsumeObserver>();
+
+        services.AddPublishObserver<LoggingPublishObserver>();
+
+        services.AddReceiveObserver<LoggingReceiveObserver>();
+
+        services.AddSendObserver<LoggingSendObserver>();
+
+        var serviceBusSettings = configuration
+            .GetSection(typeof(ServiceBusSettings).Name)
+            .Get<ServiceBusSettings>();
+
+        if (serviceBusSettings is null)
+            throw new ArgumentNullException(
+                nameof(serviceBusSettings), $"{nameof(serviceBusSettings)} is required.");
+
+        switch (serviceBusSettings.ServiceBusType)
+        {
+            case ServiceBusType.AzureServiceBus:
+                services.AddMassTransit(mt =>
+                {
+                    mt.UsingAzureServiceBus((context, cfg) =>
+                    {
+                        cfg.Host(serviceBusSettings.HostAddress);
+
+                        cfg.ConfigureEndpoints(context);
+                    });
+                });
+                break;
+            case ServiceBusType.RabbitMQ:
+                services.AddMassTransit(mt =>
+                {
+                    mt.UsingRabbitMq((context, cfg) =>
+                    {
+                        cfg.Host(serviceBusSettings.HostAddress);
+
+                        cfg.ConfigureEndpoints(context);
+                    });
+                });
+                break;
+            default:
+                throw new ArgumentException(
+                    $"Not found {nameof(serviceBusSettings.ServiceBusType)}: {serviceBusSettings.ServiceBusType}");
+        }
 
         return services;
     }
