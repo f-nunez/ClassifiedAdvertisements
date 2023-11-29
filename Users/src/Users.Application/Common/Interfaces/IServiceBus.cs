d8cf74a43d18c645d5e6fa5b4a6b4bd4cddb037b
Francisco Nuñez <fonfonferi@gmail.com>
2023-11-28 23:42:02 -0800

    IServiceBus #39

---
+++ Users/src/Users.Application/Common/Interfaces/IServiceBus.cs
@@ -0,0 +1,6 @@
+namespace Users.Application.Common.Interfaces;
+
+public interface IServiceBus
+{
+    Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default);
+}
