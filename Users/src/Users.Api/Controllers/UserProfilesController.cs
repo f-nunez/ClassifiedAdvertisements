f9e15ceab5559265ca723a34d81014cd00ce2dcc
Francisco Nuñez <fonfonferi@gmail.com>
2023-12-03 22:16:31 -0800

    UserProfilesController #39
    UpdateUserProfile method in Users.Api

--- Users/src/Users.Api/Controllers/UserProfilesController.cs
+++ Users/src/Users.Api/Controllers/UserProfilesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Users.Application.Common.Requests;
 using Users.Application.Features.UserProfiles.Commands.CreateUserProfile;
+using Users.Application.Features.UserProfiles.Commands.UpdateUserProfile;
 
 namespace Users.Api.Controllers;
 
@@ -31,9 +32,46 @@ public class UserProfilesController : BaseApiController
 
         return Ok(response);
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUserProfile(
+        string id,
+        [FromBody] UpdateUserProfileRequestParameter parameter,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateUserProfileCommand(
+            new UpdateUserProfileRequest
+            {
+                Email = parameter.Email,
+                FirstName = parameter.FirstName,
+                Id = id,
+                LastName = parameter.LastName,
+                PhoneNumber = parameter.PhoneNumber,
+                ProfileImage = parameter.ProfileImage,
+                RoleIds = parameter.RoleIds,
+                UserName = parameter.UserName
+            }
+        );
+
+        UpdateUserProfileResponse response = await Mediator
+            .Send(command, cancellationToken);
+
+        return Ok(response);
+    }
 }
 
 public class CreateUserProfileRequestParameter
+{
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? ProfileImage { get; set; }
+    public List<string> RoleIds { get; set; } = [];
+    public string? UserName { get; set; }
+}
+
+public class UpdateUserProfileRequestParameter
 {
     public string? Email { get; set; }
     public string? FirstName { get; set; }
